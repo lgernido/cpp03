4a3be19bec4bffbe9da04740f0208b75f0cda394
Lucie Gernidos <lgernido@bess-f3r1s9.clusters.42paris.fr>
2024-05-16 13:59:08 +0200

    ex01 ok

---
+++ ex01/ScavTrap.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 11:47:43 by lgernido          #+#    #+#             */
+/*   Updated: 2024/05/16 13:30:33 by lgernido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+#define RED		"\033[0;31m"
+#define GREEN	"\033[0;32m"
+#define YELLOW	"\033[0;33m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE	"\033[0;34m"
+#define MAGENTA	"\033[0;35m"
+#define CYAN	"\033[0;36m"
+#define WHITE	"\033[0;37m"
+#define BOLD       "\033[1m"
+#define UNDERLINE  "\033[4m"
+#define ITALIC     "\033[3m"
+#define RESET      "\033[0m"
+
+class ScavTrap : public ClapTrap
+{
+    public :
+        ScavTrap();
+        ~ScavTrap();
+        ScavTrap(std::string name);
+
+        void attack(const std::string& target);
+        void guardGate();
+
+
+    private : 
+        
+};
+
+#endif
